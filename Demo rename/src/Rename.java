98f350eac7a3cc62d77e3e8fd0caf15da8708297
sathish <sathish@192.168.100.5>
2018-09-01 11:12:43 +0530

    Initial commit

---
+++ Demo rename/src/Rename.java
@@ -0,0 +1,38 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Rename {
+	WebDriver driver;
+	@BeforeTest
+	public void setup() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);
+		driver.get("https://www.google.com");
+		Thread.sleep(5000);
+		System.out.println("Successfully launced browser");
+	}
+	@Test
+	public void Test4() throws InterruptedException
+	{
+		driver.findElement(By.xpath("//*[text()='Log On']")).click();
+		Thread.sleep(6000);
+		driver.findElement(By.xpath("//input[@id='j_username']")).sendKeys("abcdefgh");
+		Thread.sleep(6000);
+		driver.findElement(By.xpath("//input[@id='j_username']")).clear();
+        System.out.println("Cleared");
+	}
+	@AfterTest
+	public void teardown()
+	{
+		driver.close();
+	}	
+}
